2b955fad61d67f9fd72df4e894d9f50fa45dd056
Andrew Radev <andrey.radev@gmail.com>
2020-08-30 13:06:51 +0300

    Git completion

--- lib/custom_completion.zsh
+++ lib/custom_completion.zsh
@@ -1,3 +1,5 @@
+# For a guide, see: https://github.com/zsh-users/zsh-completions/blob/master/zsh-completions-howto.org
+
 # Escape special characters with backslashes
 # Something like this should (but doesn't) also work:
 # while read -r line; do printf "%q\n" "$line"; done
@@ -16,3 +18,17 @@ _playlist_completion() {
   reply=($(mpc lsplaylists | __escape_strings_stdin))
 }
 compctl -K _playlist_completion playlist
+
+# Git command completion
+zstyle ':completion:*:*:git:*' user-commands \
+  pull-merge:'Pulls the given branch and then goes back to the current one and merges it'
+
+_git-pull-merge() {
+  _values $(git branch | cut -c 3-)
+}
+_git-delete-branch() {
+  _values $(git branch | cut -c 3-)
+}
+
+# Possibility: all git- commands, no descriptions:
+# zstyle ':completion:*:*:git:*' user-commands ${${(M)${(k)commands}:#git-*}/git-/}
